e8c187696b8cee15d54e74262c7a9c90b7a4a181
ACE <ace@ace.com>
2018-04-11 20:57:19 +0200

    songs table

--- src/java/uk/co/queenmaryuniversity/musicrythm/model/LoginBean.java
+++ src/java/uk/co/queenmaryuniversity/musicrythm/model/LoginBean.java
@@ -10,6 +10,7 @@ import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import org.primefaces.context.RequestContext;
@@ -18,6 +19,7 @@ import org.primefaces.context.RequestContext;
  *
  * @author ACE
  */
+@ManagedBean
 @Named(value = "login")
 @SessionScoped
 public class LoginBean implements Serializable {
@@ -27,7 +29,6 @@ public class LoginBean implements Serializable {
     private String username;
     private String password;
     private Boolean rememberMe;
-    private Boolean isLoggedIn = false;
     private User user;
     private UIComponent signInbutton;
 
@@ -35,23 +36,31 @@ public class LoginBean implements Serializable {
      * Creates a new instance of LoginBean
      */
     public LoginBean() {
+        System.out.println("created----------------------");
+        System.out.println("created----------------------");
+        System.out.println("created----------------------");
+        System.out.println("created----------------------");
+        System.out.println("created----------------------");
+        System.out.println("created----------------------");
+        System.out.println("created----------------------");
+        System.out.println("created----------------------");
     }
 
     public void submit() {
-        this.user = dao.login(username, password);
+        System.out.println("SUBMITSUBMITSUBMITSUBMITSUBMITSUBMITSUBMITSUBMITSUBMITSUBMITSUBMITSUBMITSUBMITSUBMITSUBMITSUBMITSUBMITSUBMITSUBMITSUBMITSUBMIT");
+        this.setUser(dao.login(username, password));
         if (user == null) {
             FacesMessage message = new FacesMessage("Invalid credentials");
             FacesContext context = FacesContext.getCurrentInstance();
             context.addMessage(signInbutton.getClientId(context), message);
         } else {
             RequestContext.getCurrentInstance().addCallbackParam("loggedIn", true);
-            isLoggedIn = true;
         }
     }
 
     public void logout() {
+        System.out.println("LOGOUT_LOGOUT_LOGOUT_LOGOUT_LOGOUT_LOGOUT_LOGOUT_LOGOUT_LOGOUT_LOGOUT_LOGOUT_LOGOUT_LOGOUT");
         user = null;
-        isLoggedIn = false;
     }
 
     public String getUsername() {
@@ -77,20 +86,14 @@ public class LoginBean implements Serializable {
     public void setRememberMe(Boolean rememberMe) {
         this.rememberMe = rememberMe;
     }
-
-    public Boolean getIsLoggedIn() {
-        return isLoggedIn;
-    }
-
-    public void setIsLoggedIn(Boolean isLoggedIn) {
-        this.isLoggedIn = isLoggedIn;
-    }
-
+   
     public User getUser() {
         return user;
     }
 
     public void setUser(User user) {
+        System.out.println("User set  /////////////////");
+        System.out.println("//////////////////////");
         this.user = user;
     }
 
@@ -100,6 +103,12 @@ public class LoginBean implements Serializable {
 
     public void setSignInbutton(UIComponent signInbutton) {
         this.signInbutton = signInbutton;
+    }
+    
+    public boolean isLoggedIn(){
+        /*System.out.println(" logged In:"+(user != null));
+        System.out.println("*******************");*/
+        return user != null;
     }
 
 }
--- src/java/uk/co/queenmaryuniversity/musicrythm/playlists/PlaylistsLazyView.java
+++ src/java/uk/co/queenmaryuniversity/musicrythm/playlists/PlaylistsLazyView.java
@@ -6,7 +6,9 @@
 package uk.co.queenmaryuniversity.musicrythm.playlists;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
@@ -16,36 +18,81 @@ import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import org.primefaces.event.SelectEvent;
 import org.primefaces.model.LazyDataModel;
+import org.primefaces.model.SortOrder;
 import uk.co.queenmaryuniversity.musicrythm.model.LoginBean;
 import uk.co.queenmaryuniversity.musicrythm.model.MusicRhythmDAO;
 import uk.co.queenmaryuniversity.musicrythm.model.PlayList;
+import uk.co.queenmaryuniversity.musicrythm.model.Song;
 import uk.co.queenmaryuniversity.musicrythm.model.User;
- 
-@ManagedBean(name="playlistsLazyView")
+
+@ManagedBean(name = "playlistsLazyView")
 @ViewScoped
-public class PlaylistsLazyView implements Serializable {     
+public class PlaylistsLazyView implements Serializable {
+
     @EJB
     private MusicRhythmDAO dao;
-    private LazyDataModel<PlayList> lazyModel;     
-    private PlayList selectedPlaylist;
-    @ManagedProperty(value="#{login}")
-    private LoginBean  loginBean; 
-    
-     
+    private LazyDataModel<PlayList> lazyModel;
+    private LazyDataModel<PlayList> playlistSongsLazyModel;
+    private PlayList selectedPlaylist;    
+    @ManagedProperty(value = "#{login}")
+    private LoginBean loginBean;
+
     @PostConstruct
-    public void init() { 
-        User user = loginBean.getUser();        
+    public void init() {
+        User user = loginBean.getUser();
         lazyModel = new LazyPlaylistDataModel(dao.retrieveUserPlaylists(user));
     }
- 
+
     public LazyDataModel<PlayList> getLazyModel() {
         return lazyModel;
     }
- 
+
+    public LazyDataModel<Song> getPlaylistSongsLazyModel() {
+        return new LazyDataModel<Song>() {
+            @Override
+            public Song getRowData(String rowKey) {
+                for (Song song : selectedPlaylist.getSongs()) {
+                    if (song.getId().equals(Long.parseLong(rowKey))) {
+                        return song;
+                    }
+                }
+                return null;
+            }
+
+            @Override
+            public Object getRowKey(Song song) {
+                return song.getId();
+            }
+
+            @Override
+            public List<Song> load(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String, Object> filters) {
+                List<Song> data = new ArrayList<>();
+                if (selectedPlaylist != null) {
+                    data.addAll(selectedPlaylist.getSongs());
+                }
+                //rowCount
+                int dataSize = data.size();
+                this.setRowCount(dataSize);
+
+                //paginate
+                if (dataSize > pageSize) {
+                    try {
+                        return data.subList(first, first + pageSize);
+                    } catch (IndexOutOfBoundsException e) {
+                        return data.subList(first, first + (dataSize % pageSize));
+                    }
+                } else {
+                    return data;
+                }
+            }
+
+        };
+    }
+
     public void onRowSelect(SelectEvent event) {
-        FacesMessage msg = new FacesMessage("Playlist Selected", ""+((PlayList) event.getObject()).getId());
+        FacesMessage msg = new FacesMessage("Playlist Selected", "" + ((PlayList) event.getObject()).getId());
         FacesContext.getCurrentInstance().addMessage(null, msg);
-    }       
+    }
 
     public LoginBean getLoginBean() {
         return loginBean;
@@ -62,18 +109,19 @@ public class PlaylistsLazyView implements Serializable {
     public void setSelectedPlaylist(PlayList selectedPlaylist) {
         this.selectedPlaylist = selectedPlaylist;
     }
-    
-    
+
     public void deletePlaylist(PlayList playlist) {
-        System.out.println("user playlist:"+loginBean.getUser().getPlaylists().size());
-        dao.deletePlaylist(playlist);           
-        final List<PlayList> playlistsDatasource = ((LazyPlaylistDataModel)lazyModel).getDatasource();        
-        System.out.println("playlists #:"+playlistsDatasource.size());        
-        playlistsDatasource.remove(playlist);        
-        System.out.println("playlists #:"+playlistsDatasource.size());
+        System.out.println("user playlist:" + loginBean.getUser().getPlaylists().size());
+        dao.deletePlaylist(playlist);
+        final List<PlayList> playlistsDatasource = ((LazyPlaylistDataModel) lazyModel).getDatasource();
+        System.out.println("playlists #:" + playlistsDatasource.size());
+        playlistsDatasource.remove(playlist);
+        System.out.println("playlists #:" + playlistsDatasource.size());
     }
-    
-    
-    
+
+    public void deleteSong(Song song){
+        selectedPlaylist.getSongs().remove(song);
+        System.out.println("song removed");
+    }
     
 }
